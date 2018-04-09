e95700db2afb217ac7326a0cbf893d60280beff8
isanghyun <21600499@handong.edu>
2018-04-09 11:18:45 +0900

    TimeRecorder

--- src/main/java/edu/handong/csee/java/lecture/multidimensionalarray/TimeRecorder.java
+++ src/main/java/edu/handong/csee/java/lecture/multidimensionalarray/TimeRecorder.java
@@ -16,8 +16,7 @@ public class TimeRecorder {
 		
 		myTimeRecoder.getData();
 		
-		myTimeRecoder.computeTotalPerWeekDay();
-		myTimeRecoder.computeTotalPerEmployee();
+		myTimeRecoder.computeTotals();
 		
 		myTimeRecoder.printResults();
 
@@ -49,7 +48,7 @@ public class TimeRecorder {
 		myScanner.close();	
 	}
 	
-	public void computeTotalPerWeekDay() {
+	public void computeTotals() {
 			
 		for(WeekDays currentDay:WeekDays.values()) {
 			
@@ -58,26 +57,12 @@ public class TimeRecorder {
 			for(int employeeCount=0; employeeCount < hours.length; employeeCount++) {	
 				dayHours[currentDay.ordinal()] = dayHours[currentDay.ordinal()] 
 															+ hours[employeeCount][currentDay.ordinal()];
+				
 			}
 		}
 	}
 	
-	public void computeTotalPerEmployee() {
-		
-		weekHours = new int[hours.length];
-		
-		for(int employeeCount=0; employeeCount < hours.length; employeeCount++) {
-			
-			weekHours[employeeCount] = 0;
-		
-			for(WeekDays currentDay:WeekDays.values()) {
-				weekHours[employeeCount] = weekHours[employeeCount] 
-															+ hours[employeeCount][currentDay.ordinal()];
-			}
-		}
-		
-	}
-	
+
 	public void printResults() {
 		
 		System.out.println();
